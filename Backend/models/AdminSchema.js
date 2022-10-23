f2df78b64d30abbab1a0c1c9a348e03a0b4cddd9
RadwanWalid <rado.king2001@gmail.com>
2022-10-23 15:20:20 +0200

    initial

---
+++ Backend/models/AdminSchema.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const connection = require('../config/database');
+
+const AdminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required:true,
+    unique:true
+   },
+   email:{
+     type: String,
+     required:true,
+     unique:true 
+   },
+   hash:{
+     type: String,
+     required:true,
+   } ,
+   salt:{
+     type: String,
+     required:true,
+   } ,
+   firstName:{
+     type: String,
+     required:true,
+   } ,
+   lastName: {
+     type: String,
+     required:true,
+   },
+
+
+
+
+
+  //courses 
+  // refrence embeeded  , 
+  /* 
+  1)one to one prefeer embeeded ,
+  2)one to few 50 100 prefer embeeded
+  3)one to many prefeer refrence
+  4)many to many refer
+  
+  */
+
+
+  //  individ,corp,admin,instruct 
+
+});
+
+/* 
+*/
+
+const Admin = connection.model('Admin', AdminSchema);
+
+
+module.exports = Admin;
---
+++ Backend/models/CourseSchema.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const connection = require('../config/database');
+
+const CourseSchema = new mongoose.Schema({
+  title: 
+  {type:String,
+    required:true,
+    unique: true,
+  },
+  subtitle:
+  {type:String,
+    required:true
+  },
+  summary: 
+  {type:String,
+    required:true
+  },
+  price: 
+  {type:Number,
+    required:true
+  },
+  subject:  {type:String,
+    required:true
+  },
+  instructor: { id: String, name: String },
+  rating:
+  {type:Number
+  },
+
+
+
+
+
+
+
+  //courses 
+  // refrence embeeded  , 
+  /* 
+  1)one to one prefeer embeeded ,
+  2)one to few 50 100 prefer embeeded
+  3)one to many prefeer refrence
+  4)many to many refer
+  
+  */
+
+
+  //  individ,corp,admin,instruct 
+
+});
+
+/* 
+*/
+
+const Course = connection.model('Course', CourseSchema);
+
+
+module.exports = Course;
