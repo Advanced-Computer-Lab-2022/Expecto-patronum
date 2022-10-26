20f62dde4c10a5a9b527f444b94bcd461bcbfd6b
RadwanWalid <rado.king2001@gmail.com>
2022-10-26 05:04:11 +0200

    navbar 1.0

--- Frontend/components/SearchBar/SearchBar.tsx
+++ Frontend/components/SearchBar/SearchBar.tsx
@@ -6,9 +6,9 @@ type Props = {}
 const SearchBar = (props: Props) => {
   return (
     <form className='flex items-center'>
-        <input className='rounded-full w-52 h-8 bg-navlink-bg' placeholder='Start Hacking'/>
+        <input className='rounded-full text-white w-96 h-8 pl-2.5 bg-navlink-bg placeholder:italic placeholder:text-sm placeholder-white tracking-wide placeholder-opacity-70 transition-outline-width duration-200 focus:outline focus:outline-2 outline-searchFocus' placeholder='Search for anything'/>
         <button type='submit'>
-            <BiSearchAlt2 className='text-white z-10 scale-150 relative right-7 rotate-90' />
+            <BiSearchAlt2 className='text-white z-10 scale-150 hover:scale-160 transition-all duration-200 relative right-7 rotate-90' />
         </button>
     </form>
   )
