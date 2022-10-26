18cbd1b18d4c6892822fc1adbd3f58cf58bcdad3
RadwanWalid <rado.king2001@gmail.com>
2022-10-26 17:02:00 +0200

    navbar 1.1

--- Frontend/components/SearchBar/SearchBar.tsx
+++ Frontend/components/SearchBar/SearchBar.tsx
@@ -6,9 +6,9 @@ type Props = {}
 const SearchBar = (props: Props) => {
   return (
     <form className='flex items-center'>
-        <input className='rounded-full text-white w-96 h-8 pl-2.5 bg-navlink-bg placeholder:italic placeholder:text-sm placeholder-white tracking-wide placeholder-opacity-70 transition-outline-width duration-200 focus:outline focus:outline-2 outline-searchFocus' placeholder='Search for anything'/>
-        <button type='submit'>
-            <BiSearchAlt2 className='text-white z-10 scale-150 hover:scale-160 transition-all duration-200 relative right-7 rotate-90' />
+        <input className='rounded-full text-white nv-max:hidden w-96 h-8 pl-2.5 bg-navlink-bg placeholder:italic placeholder:text-sm placeholder-white tracking-wide placeholder-opacity-70 transition-outline-width duration-200 focus:outline focus:outline-2 outline-searchFocus' placeholder='Search for anything'/>
+        <button className='rounded-full nv-max:bg-white bg-navlink-bg p-2 relative right-8' type='submit'>
+            <BiSearchAlt2 className='text-white z-10 nv-max:text-navlink-bg scale-150 hover:scale-160 transition-all duration-200 rotate-90' />
         </button>
     </form>
   )
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -16,7 +16,18 @@ module.exports = {
         '160': '1.60',
       },
       transitionProperty: {
-        'outline-width': 'outline-width'
+        'outline-width': 'outline-width',
+        'width': 'width',
+        'display': 'display',
+      },
+      screens: {
+        'nv': '934px',
+        '2xl-max': {'max': '1535px'},
+        'xl-max': {'max': '1279px'},
+        'lg-max': {'max': '1023px'},
+        'md-max': {'max': '767px'},
+        'sm-max': {'max': '639px'},
+        'nv-max': {'max': '935px'},
       }
     },
   },
