ed289309db1e0bf032a1386811ce6aafb448f81a
RadwanWalid <rado.king2001@gmail.com>
2022-10-28 01:15:38 +0200

    1.2

--- Frontend/components/SearchBar/SearchBar.tsx
+++ Frontend/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { BiSearchAlt2 } from "react-icons/bi";
 
 type Props = {}
 
 const SearchBar = (props: Props) => {
+
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [type, setType] = useState<"submit" | "button" | "reset" | undefined>(global.innerWidth < 935 ? "button" : "submit");
+  const [isResponsiveOn, setIsResponsiveOn] = useState(global.innerWidth < 935 ? true : false);
+  const [isSearchToggled, setIsSearchToggled] = useState(false);
+  const searchRef = useRef<any>();
+  const searchInputRef = useRef<any>();
+
+  useEffect(() => {
+
+    if(type === "submit") {
+      if(searchRef !== undefined) {
+        if(searchValue === "") {
+          setIsEmpty(true);
+          searchInputRef.current.style.display = "initial";
+          searchInputRef.current.style.left = "0px";
+          searchInputRef.current.style.top = "0px";
+          searchRef.current.style.cursor = "not-allowed";
+        }
+        else {
+          setIsEmpty(false);
+          searchInputRef.current.style.display = "none";
+          searchInputRef.current.style.top = "65px";
+          searchRef.current.style.cursor = "initial";
+        }
+      }
+    } else {
+      setIsEmpty(false);
+      searchInputRef.current.style.display = "none";
+      searchInputRef.current.style.top = "65px";
+      searchRef.current.style.cursor = "initial";
+    }
+
+    
+  }, [searchValue, type])
+
+  global.onresize = () => {
+    setIsResponsiveOn(global.innerWidth < 935 ? true : false);
+    setType(global.innerWidth < 935 ? "button" : "submit");
+  }
+
+  global.onload = () => {
+    setIsResponsiveOn(global.innerWidth < 935 ? true : false);
+    setType(global.innerWidth < 935 ? "button" : "submit");
+  }
+
+  function toggleSearchBar(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if(isResponsiveOn) {
+      if(!isSearchToggled) {
+        searchInputRef.current.style.display = "initial";
+        searchInputRef.current.style.top = "65px";
+        setIsSearchToggled(!isSearchToggled);
+      } else {
+        searchInputRef.current.style.display = "none";
+        searchInputRef.current.style.left = "0px";
+        searchInputRef.current.style.top = "0px";
+        setIsSearchToggled(!isSearchToggled);
+      }
+      
+      e.preventDefault();
+    }
+  }
+
   return (
-    <form className='flex items-center'>
-        <input className='rounded-full text-white nv-max:hidden w-96 h-8 pl-2.5 bg-navlink-bg placeholder:italic placeholder:text-sm placeholder-white tracking-wide placeholder-opacity-70 transition-outline-width duration-200 focus:outline focus:outline-2 outline-searchFocus' placeholder='Search for anything'/>
-        <button className='rounded-full nv-max:bg-white bg-navlink-bg p-2 relative right-8' type='submit'>
-            <BiSearchAlt2 className='text-white z-10 nv-max:text-navlink-bg scale-150 hover:scale-160 transition-all duration-200 rotate-90' />
+    <form className='flex items-center w-full'>
+        <input ref={searchInputRef} id='search-value' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className='rounded-full nv-max:absolute relative nv-max:hidden max-w-lg nv-max:w-11/12 nv-max:mx-auto text-white nv-max:left-0 nv-max:right-0 w-96 h-8 pl-2.5 bg-navlink-bg placeholder:italic placeholder:text-sm placeholder-white tracking-wide placeholder-opacity-70 transition-outline-width duration-200 focus:outline focus:outline-2 outline-searchFocus' placeholder='Search for anything'/>
+        <button ref={searchRef} id='submit-search-btn' className='rounded-full nv-max:absolute nv-max:right-24 nv-max:bg-white bg-navlink-bg p-2 relative right-8' type={type} disabled={isEmpty} onClick={(e) => toggleSearchBar(e)}>
+            <BiSearchAlt2 className='text-white nv-max:text-navlink-bg scale-150 hover:scale-160 transition-all duration-200 rotate-90' />
         </button>
     </form>
   )
--- Frontend/components/shared/Navbar/Navbar.tsx
+++ Frontend/components/shared/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import SearchBar from "../../SearchBar/SearchBar";
+import BurgerButton from "./BurgerButton/BurgerButton";
 
 function Navbar() {
+
+    const curtainRef = useRef<any>();
+    const [isCurtainOpen, setIsCurtainOpen] = useState(false);
 
     function autoMove(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
         
@@ -10,7 +14,9 @@ function Navbar() {
         if(movableHover != undefined) {
             movableHover.style.display = "initial";
             movableHover.style.left = e.currentTarget.offsetLeft.toString() + "px";
+            movableHover.style.top = (e.currentTarget.offsetTop + 10).toString() + "px";
             movableHover.style.width = e.currentTarget.offsetWidth.toString() + "px";
+            movableHover.style.height = (e.currentTarget.offsetHeight - 20).toString() + "px";
         }
     }
 
@@ -24,18 +30,35 @@ function Navbar() {
         }
     }
 
+    function toggleCurtain() {
+        if(curtainRef != undefined) {
+            if(!isCurtainOpen) {
+                curtainRef.current.style.bottom = "-104px";
+                curtainRef.current.style.opacity = "100%";
+            } else {
+                curtainRef.current.style.bottom = "9rem";
+                curtainRef.current.style.opacity = "0%";
+            }
+        }
+        setIsCurtainOpen(!isCurtainOpen);
+    }
+
+
     return (
-        <div className="flex justify-between items-center h-16 drop-shadow-lg shadow-lg bg-navbar">
-            <div className="flex" >
-                <img className="h-20 py-3 px-6" src="images/Expecto Patronum (White).png" />
+        <div className="flex z-10 justify-between items-center nv:px-2 h-16 drop-shadow-lg shadow-lg bg-navbar">
+            <div className="nv-max:absolute flex">
+                <img className="h-20 min-w-fit py-3 px-6" src="images/Expecto Patronum (White).png" />
+            </div>
+            <div className="z-20">
                 <SearchBar />
+                <BurgerButton toggle={toggleCurtain} />
             </div>
-            <div className="pr-14 flex justify-between items-center h-full" onMouseLeave={hide}>
-                <a className="text-navlink py-3 px-4 hover:scale-105 transition-all duration-300" href="" onMouseOver={(e) => autoMove(e)}>Services</a>
-                <a className="text-navlink py-3 px-4 hover:scale-105 transition-all duration-300" href="" onMouseOver={(e) => autoMove(e)}>Log in</a>
-                <a className="text-navlink py-3 px-4 hover:scale-105 transition-all duration-300" href="" onMouseOver={(e) => autoMove(e)}>Sign up</a>
+            <div ref={curtainRef} className="nv-max:relative z-last nv-max:bottom-36 transition-bottom transition-opacity duration-1000 nv-max:w-screen nv-max:bg-navbar" onMouseLeave={hide} onClick={toggleCurtain} >
+                <a className="text-navlink py-3 px-4 whitespace-nowrap z-10 relative nv-max:block transition-all duration-300" href="" onMouseOver={(e) => autoMove(e)}>Services</a>
+                <a className="text-navlink py-3 px-4 whitespace-nowrap z-10 relative nv-max:block transition-all duration-300" href="" onMouseOver={(e) => autoMove(e)}>Log in</a>
+                <a className="text-navlink py-3 px-4 whitespace-nowrap z-10 relative nv-max:block transition-all duration-300" href="" onMouseOver={(e) => autoMove(e)}>Sign up</a>
+                <div id="movable-hover" className="absolute hidden right-0 transition-all nv:z-behind nv-max:z-0 duration-200 bg-navlink-bg h-6 px-4 py-1 rounded-full"></div>
             </div>
-            <div id="movable-hover" style={{zIndex: "-1", transition: "all 0.2s"}} className="absolute hidden z-0 bg-navlink-bg h-6 px-4 py-1 rounded-full"></div>
         </div>
     );
 }
