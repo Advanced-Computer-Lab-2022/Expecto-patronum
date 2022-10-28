cfe2d1a2cbe53c67ee2659198f5f92eaf25198b4
RadwanWalid <rado.king2001@gmail.com>
2022-10-28 22:55:28 +0200

    footer 1.1

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import Footer from "../components/shared/Footer/Footer";
 import Navbar from "../components/shared/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Navbar />
+      <img style={{width: "100%"}} src="/images/3azama.jpg" />
       <Footer />
     </div>
   );
