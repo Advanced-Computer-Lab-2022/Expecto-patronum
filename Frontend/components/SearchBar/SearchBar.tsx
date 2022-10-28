3dd445d6275fa0b8404874e1842a61ca3d8c7388
RadwanWalid <rado.king2001@gmail.com>
2022-10-28 05:49:20 +0200

    navbar 1.3

--- Frontend/components/SearchBar/SearchBar.tsx
+++ Frontend/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { BiSearchAlt2 } from "react-icons/bi";
+import { GrFormClose, GrClose } from "react-icons/gr";
+import  { AiOutlineClose } from "react-icons/ai";
 
 type Props = {}
 
 const SearchBar = (props: Props) => {
 
-  const [isEmpty, setIsEmpty] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
   const [searchValue, setSearchValue] = useState("");
   const [type, setType] = useState<"submit" | "button" | "reset" | undefined>(global.innerWidth < 935 ? "button" : "submit");
   const [isResponsiveOn, setIsResponsiveOn] = useState(global.innerWidth < 935 ? true : false);
@@ -15,31 +17,31 @@ const SearchBar = (props: Props) => {
 
   useEffect(() => {
 
-    if(type === "submit") {
-      if(searchRef !== undefined) {
-        if(searchValue === "") {
-          setIsEmpty(true);
-          searchInputRef.current.style.display = "initial";
-          searchInputRef.current.style.left = "0px";
-          searchInputRef.current.style.top = "0px";
-          searchRef.current.style.cursor = "not-allowed";
-        }
-        else {
-          setIsEmpty(false);
-          searchInputRef.current.style.display = "none";
-          searchInputRef.current.style.top = "65px";
-          searchRef.current.style.cursor = "initial";
-        }
+    if(isResponsiveOn) {
+      searchRef.current.style.cursor = "initial";
+      searchInputRef.current.style.display = "none";
+      setIsDisabled(false);
+    } else {
+      const closeBtn =  document.getElementById("close-btn");
+      if(closeBtn != undefined)
+        closeBtn.style.display = "none";
+      searchInputRef.current.style.top = "initial";
+      searchInputRef.current.style.display = "initial";
+    }
+    
+  }, [isResponsiveOn])
+
+  function disableCursor() {
+    if(!isResponsiveOn || (searchInputRef.current.style.display === "initial" && isResponsiveOn)) {
+      if(searchValue == "") {
+        searchRef.current.style.cursor = "not-allowed";
+      } else {
+        searchRef.current.style.cursor = "initial";
       }
     } else {
-      setIsEmpty(false);
-      searchInputRef.current.style.display = "none";
-      searchInputRef.current.style.top = "65px";
       searchRef.current.style.cursor = "initial";
     }
-
-    
-  }, [searchValue, type])
+  }
 
   global.onresize = () => {
     setIsResponsiveOn(global.innerWidth < 935 ? true : false);
@@ -51,27 +53,49 @@ const SearchBar = (props: Props) => {
     setType(global.innerWidth < 935 ? "button" : "submit");
   }
 
-  function toggleSearchBar(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    if(isResponsiveOn) {
-      if(!isSearchToggled) {
-        searchInputRef.current.style.display = "initial";
-        searchInputRef.current.style.top = "65px";
-        setIsSearchToggled(!isSearchToggled);
-      } else {
-        searchInputRef.current.style.display = "none";
-        searchInputRef.current.style.left = "0px";
-        searchInputRef.current.style.top = "0px";
-        setIsSearchToggled(!isSearchToggled);
+  function toggleSearchInput(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+
+    const closeBtn = document.getElementById("close-btn");
+
+    if(type === "button") {
+      //View Search Input with Close button
+      searchInputRef.current.style.display = "initial";
+      searchInputRef.current.style.top = "85px";
+      if(closeBtn != undefined) {
+        closeBtn.style.display = "initial";
       }
-      
+
+      setType("submit");
       e.preventDefault();
-    }
+    } else if (!isResponsiveOn && type === "submit"){
+      console.log("entered")
+      if(searchValue === "") {
+        setIsDisabled(true);
+      } else {
+        setIsDisabled(false);
+      }
+      e.preventDefault();
+    } 
+
+  }
+
+  function closeSearch() {
+    if(searchInputRef != undefined)
+      searchInputRef.current.style.display = "none";
+
+    const closeBtn = document.getElementById("close-btn");
+
+    if(closeBtn != undefined)
+      closeBtn.style.display = "none";
+
+    setType("button");
   }
 
   return (
     <form className='flex items-center w-full'>
         <input ref={searchInputRef} id='search-value' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className='rounded-full nv-max:absolute relative nv-max:hidden max-w-lg nv-max:w-11/12 nv-max:mx-auto text-white nv-max:left-0 nv-max:right-0 w-96 h-8 pl-2.5 bg-navlink-bg placeholder:italic placeholder:text-sm placeholder-white tracking-wide placeholder-opacity-70 transition-outline-width duration-200 focus:outline focus:outline-2 outline-searchFocus' placeholder='Search for anything'/>
-        <button ref={searchRef} id='submit-search-btn' className='rounded-full nv-max:absolute nv-max:right-24 nv-max:bg-white bg-navlink-bg p-2 relative right-8' type={type} disabled={isEmpty} onClick={(e) => toggleSearchBar(e)}>
+        <AiOutlineClose id='close-btn' className='absolute text-red-500 hidden right-44 top-18 hover:scale-110 hover:cursor-pointer transition-transform duration-300' onClick={closeSearch} />
+        <button ref={searchRef} id='submit-search-btn' className='rounded-full nv-max:absolute nv-max:right-24 nv-max:bg-white bg-navlink-bg p-2 relative right-8' type={type} disabled={isDisabled} onMouseOver={disableCursor} onClick={(e) => toggleSearchInput(e)}>
             <BiSearchAlt2 className='text-white nv-max:text-navlink-bg scale-150 hover:scale-160 transition-all duration-200 rotate-90' />
         </button>
     </form>
