b3e2961229ca8c662442443c8c024c477f71e32f
davidsamy1 <99022820+davidsamy1@users.noreply.github.com>
2022-10-31 20:14:36 +0200

    Admin Add Users/CountryPicker/Sidebar

--- Frontend/components/SideBar/SideBar.tsx
+++ Frontend/components/SideBar/SideBar.tsx
@@ -46,7 +46,7 @@ const showNav = (flag: any) => {
          <img src="images/Expecto Patronum (White).png" className="mr-3 h-6 sm:h-7" alt="Flowbite Logo" />
          <span className="self-center text-m font-semibold whitespace-nowrap dark:text-white">Rodin Salem</span>
       </a>
-       <ul className="space-y-1.5">
+       <ul className="space-y-1">
           <li>
              <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                 <svg aria-hidden="true" className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
@@ -92,7 +92,7 @@ const showNav = (flag: any) => {
              </a>
           </li>
        </ul>
-       <ul className="pt-4 mt-4 space-y-1.5 border-t border-gray-200 dark:border-gray-700">
+       <ul className="pt-4 mt-4 space-y-1 border-t border-gray-200 dark:border-gray-700">
           <li>
              <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
                 <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white dark:text-gray-400" focusable="false" data-prefix="fas" data-icon="gem" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z"></path></svg>
--- Frontend/pages/AdminAddUser.tsx
+++ Frontend/pages/AdminAddUser.tsx
@@ -6,16 +6,22 @@ type Props = {}
 
 var isReset = false;
 var response = null;
+var radio: null=null;
 
 const AddUser = (props: Props) => {
   const [characterLeft, setCharacterLeft] = useState(250);
-  const [index, setIndex] = useState<number>(0);
+  const [selectedRadio, setSelectedRadio] = useState<string>("");
   const [type, setType] = useState<"button" | "submit" | "reset" | undefined>("button");
+  const radioRef = useRef<any>();
+
+  const username = useRef<any>();
+  const email = useRef<any>();
+  const password = useRef<any>();
+  const firstname = useRef<any>();
+  const lastname = useRef<any>();
 
 
-  const labelRef = useRef<any>();
-
-  function resetError(e: React.FormEvent<HTMLFormElement>) {
+  function resetError(e: React.FormEvent<HTMLFormElement> ) {
     const error = document.getElementById("error-message");
 
     if(error != undefined)
@@ -23,51 +29,54 @@ const AddUser = (props: Props) => {
 
     isReset = true;
 }
-  function moveLabel() {
-    labelRef.current.style.bottom = "9rem";
-    labelRef.current.style.color = "rgb(255, 255, 255)";
-    labelRef.current.style.fontSize = "0.86rem";
-    labelRef.current.style.transition = "font-size 1s, bottom 1s, color 0.5s linear 0.3s";
-  }
 
-  function returnToInitial(e: React.FocusEvent<HTMLTextAreaElement, Element>) {
-    if (e.target.value === "") {
-      labelRef.current.style.bottom = "8rem";
-      labelRef.current.style.color = "rgb(156, 163, 175)";
-      labelRef.current.style.fontSize = "initial";
-      labelRef.current.style.transition = "font-size 1s, bottom 1s, color 0.5s linear 0.3s";
-    }
-  }
+  async function createUser(e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement, MouseEvent>) {
 
-  async function createUser(e: React.FormEvent<HTMLFormElement>) {
-
-    const inputFields = document.getElementsByClassName("create-user-input") as HTMLCollectionOf<HTMLElement>;
+    const inputFields = document.getElementsByClassName("create-course-input") as HTMLCollectionOf<HTMLElement>;
     var missing = false;
-    if(index === 0) {
-        for(let i = 0; i < inputFields.length; i++) {
-            const field = inputFields[i] as HTMLTextAreaElement | HTMLInputElement;
-            if(field.value === "") {
-                const label = document.getElementsByClassName("create-user-input-label")[i] as HTMLLabelElement;
-                label.style.color = "#B91C1C";
-                missing = true;
+    for(let i = 0; i < inputFields.length; i++) {
+      const field = inputFields[i] as HTMLTextAreaElement | any;
+      if(field.nodeName === "DIV") {
+          var selected = false;
+          // console.log(field.children[1].children.length);
+          for(let j = 0; j < field.children[1].children.length; j++) {
+
+            // console.log(field.children[1].children[j].children[0]);
+
+            if(field.children[1].children[j].children[0].checked) {
+                radio=field.children[1].children[j].children[2].innerHTML.replace(" ", "");
+                selected = true;
+                break;
             }
         }
-        const error = document.getElementById("error-message");
-        if(missing) {
-            if(error != undefined)
-                error.innerHTML = "Please fill in the required fields marked with a '*'.";
-            isReset = false;
-            return;
-        }
-    }
+          if(!selected) {
+              field.children[0].style.color = "#B91C1C";
+              missing = true;
+          }
+      } else {
+          if(field.value === "") {
+              const label = document.getElementsByClassName("create-course-input-label")[i] as HTMLLabelElement;
+              label.style.color = "#B91C1C";
+              missing = true;
+          }
+      }
+  }
+  const error = document.getElementById("error-message");
+  if(missing) {
+      if(error != undefined)
+          error.innerHTML = "Please fill in the required fields marked with a '*'.";
+      isReset = false;
+      return;
+  }
     e.preventDefault();
     axios.defaults.withCredentials = true;
     response = await axios.post("http://localhost:5000/Auth/register", {
-      username: (inputFields[0] as HTMLTextAreaElement | HTMLInputElement).value,
-      password: (inputFields[1] as HTMLTextAreaElement | HTMLInputElement).value,
-      email: (inputFields[2] as HTMLTextAreaElement | HTMLInputElement).value,
-      firstname: (inputFields[3] as HTMLTextAreaElement | HTMLInputElement).value,
-      lastname: (inputFields[4] as HTMLTextAreaElement | HTMLInputElement).value,
+      username: username.current.value,
+      password: password.current.value,
+      email: email.current.value,
+      firstname: firstname.current.value,
+      lastname: lastname.current.value,
+      role: radio,
     }).then((res: { data: any; }) => { return res.data });
 
     console.log(response);
@@ -75,29 +84,32 @@ const AddUser = (props: Props) => {
   return (
     <div className="flex">
       <SideBar></SideBar>
-      <form id='course-form' className='row mx-4 p-20r ' onSubmit={(e) => createUser(e)} onChange={(e) => !isReset ? resetError(e): {}}>
-        <div className='row tab mx-auto pt-10 bg-navbar w-700 rounded-t-2xl shadow-xl justify-self-center'>
+      <form id='course-form' className='w-full mx-4 p-20r'>
+        <div className='row tab mx-auto pt-10 bg-navbar h-full w-full rounded-t-2xl shadow-xl '>
           <h1 className='text-center text-3xl pb-6 text-white'>Add User</h1>
           <div className="col">
-            <Input required={true} placeholder={"User Name *"} />
-            <Input required={true} placeholder={"Password *"} />
-            <Input required={true} placeholder={"Email"} />
+            <Input ref={username} required={true} placeholder={"User Name *"} />
+            <Input ref={password} required={true} placeholder={"Password *"} />
+            <Input ref={email} required={true} placeholder={"Email *"} />
 
           </div>
-          <div className="col">
-            <Input required={true} placeholder={"First Name *"} />
-            <Input required={true} placeholder={"Last Name *"} />
+          <div className="col-md-6 col-sm-8 px-4">
+    
+            <Input ref={firstname} required={true} placeholder={"First Name *"} />
+            <Input ref={lastname} required={true} placeholder={"Last Name *"} />
+            <Input ref={radioRef} type='radio' title='User Type*' enum={['Admin', 'Instructor', 'CorporateTrainee']} required={true} placeholder={"User Type"} />
+           
           </div>
           <div className='mx-auto w-700 rounded-b-2xl bg-navbar'>
             <p id='error-message' className='text-red-700 h-auto mb-2 text-center'></p>
             <div className='text-center flex justify-center '>
-              <button id='submit-btn' type={type} className=" text-lg hover:bg-navlink-bg hover:text-gray-900 hover:rounded-md h-10 mb-4 items-center py-2 px-4 ml-3 font-medium text-navlink-bg bg-transparent">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                Add User
-              </button>
+            <button id='submit-btn' type="button" onClick={(e) => createUser(e)} className="text-lg hover:bg-navlink-bg hover:text-gray-900 hover:rounded-md h-10 mb-4 items-center py-2 px-4 ml-3 font-medium text-navlink-bg bg-transparent">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    Add User
+                </button>
             </div>
           </div>
         </div>
