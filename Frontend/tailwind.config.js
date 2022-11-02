2ce1f5e00146b56214c485936a899a89d2dab974
davidsamy1 <99022820+davidsamy1@users.noreply.github.com>
2022-11-02 17:30:23 +0200

    admin sidebar edited

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
+  plugins: [
+    require('flowbite/plugin')
+],
   theme: {
     extend: {
       colors: {
