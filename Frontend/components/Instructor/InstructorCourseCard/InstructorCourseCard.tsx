f8e3baf42b3ced801637ad1a2410e62930e32e74
RadwanWalid <rado.king2001@gmail.com>
2022-12-03 01:42:46 +0200

    Add discount 1.0

--- Frontend/components/Instructor/InstructorCourseCard/InstructorCourseCard.tsx
+++ Frontend/components/Instructor/InstructorCourseCard/InstructorCourseCard.tsx
@@ -21,7 +21,7 @@ const InstructorCourseCard = (props: Props) => {
             <p className='text-2xl my-2 pl-2 -indent-2'>{props.course.subject}</p>
             <p className='opacity-70 mb-auto'>{parseInt(props.course.courseHours)} HOURS</p>
         </div>
-        <div className={`${!props.isViewList ? 'mb-10': 'sb-max:mb-10'} px-8 flex w-full min-w-0 justify-between h-full`}>
+        <div className={`${!props.isViewList ? 'mb-10': 'sb-max:mb-10 mb-10'} px-8 flex w-full min-w-0 justify-between h-full`}>
             <div className='py-4 space-y-2 min-w-0'>
                 <div className={`${!props.isViewList ? '': 'sb:mr-22'}`}>
                     <p className='opacity-50'>TITLE</p>
@@ -32,8 +32,8 @@ const InstructorCourseCard = (props: Props) => {
             </div>
         </div>
         <a className='whitespace-nowrap absolute bottom-5 right-5 rounded-full bg-input text-white shadow-md hover:shadow-lg border-1.5 border-input hover:bg-white hover:text-input transition-all duration-300 px-2 py-0.5'>Open Course</a>
-        <label className={`${!props.isViewList ? 'bottom-6 left-5': 'sb-max:bottom-6 sb-max:left-5 sb:top-1.5 sb:right-28'} whitespace-nowrap absolute text-sm font-bold text-[#038470] bg-[#D7FFE0] px-2 py-0.5 rounded-full`}>{Math.floor(props.course.price * Rate.rate).toLocaleString()} {Rate.curr}</label>
-        <a href='/Instructor/Settings/AddDiscount/' title='Add Promotion' className={`${(!props.isViewList ? 'bottom-5 left-4': 'sb:bottom-5 sb:right-5')} whitespace-nowrap absolute rounded-full text-input bg-white shadow-md hover:shadow-lg border-1.5 border-input hover:text-white hover:bg-input transition-all duration-300 px-2 py-0.5`}>Add Discount</a>
+        <label className={`${!props.isViewList ? 'top-34 right-4': 'sb-max:top-34 sb-max:right-4 sb:top-1.5 sb:right-28'} whitespace-nowrap absolute text-sm font-bold text-[#038470] bg-[#D7FFE0] px-2 py-0.5 rounded-full`}>{Math.floor(props.course.price * Rate.rate).toLocaleString()} {Rate.curr}</label>
+        <a href='/Instructor/Settings/AddDiscount/' title='Add Promotion' className={`${(!props.isViewList ? 'bottom-5 left-4': 'sb:bottom-5 sb:right-38 sb-max:bottom-5 sb-max:left-4')} whitespace-nowrap absolute rounded-full text-input bg-white shadow-md hover:shadow-lg border-1.5 border-input hover:text-white hover:bg-input transition-all duration-300 px-2 py-0.5`}>Add Discount</a>
         <OneStar className='bg-white rounded-tr-xl rounded-bl-xl absolute top-0 right-0 py-2 px-3 shadow-md hover:shadow-sm transition-all duration-200' rating={props.course.rating === undefined ? 3.8: props.course.rating.avg} />
     </div>
   )
