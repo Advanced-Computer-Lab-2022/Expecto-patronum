6c91fa2e741ca4517b51401d6f7b22ecac8b2617
RadwanWalid <rado.king2001@gmail.com>
2022-12-10 02:25:23 +0200

    Aftermath 2.0

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -13,9 +13,11 @@ const Home: NextPage = () => {
 
   useEffect(()=> {
     Array.from(homePageImageRef.current.children).map((image: any) => {
-      image.classList.remove('-right-[48rem]');
-      image.classList.add('-right-26');
+      image.classList.remove('-right-[40rem]');
+      image.classList.add('-right-30');
+      image.classList.add('nv-max:-right-10');
     });
+    
     homePageImageBackgroundRef.current.classList.remove('opacity-0');
     homePageImageBackgroundRef.current.classList.add('opacity-100');
 
@@ -24,24 +26,23 @@ const Home: NextPage = () => {
   }, [])
 
   return (
-    <div className="relative text-center h-screen overflow-hidden">
-      <section className="flex items-center justify-between mt-16">
+    <div className="relative text-center overflow-hidden">
+      <section className="flex nv-max:flex-col-reverse items-center justify-between py-8 mx-10">
         
-        <div ref={homePageHeaderRef} className="text-left ml-20 w-[48rem] opacity-0 transition-all duration-[1500ms]">
-          <h1 className="text-5xl font-bold -indent-32 pl-32 leading-[4.5rem]">Learn a New Skill Everyday, <br /> Anytime, and Anywhere.</h1>
-            <br />
-            <p className="pl-4 text-xl mx-8">
+        <div ref={homePageHeaderRef} className="space-y-6 text-left nv-max:text-center opacity-0 transition-all duration-[1500ms]">
+          <h1 className="text-5xl nv-max:text-3xl nv-max:-indent-0 nv-max:pl-0 font-bold -indent-32 pl-32 leading-[4.5rem]">Learn a New Skill Everyday, <br /> Anytime, and Anywhere.</h1>
+            <p className="text-xl nv-max:text-lg">
               1000+ Courses covering all tech domains for you to learn and explore
               new opportunities. Learn from Industry Experts and land your Dream
               Job.
             </p>
         </div>
-
-        <div className="w-[50%] h-96 pointer-events-none">
-          <div ref={homePageImageBackgroundRef} className="flex absolute right-20 scale-120 items-center justify-center w-96 h-96 bg-canadian-red rounded-full opacity-0 transition-all duration-[1500ms]">
-            <div className="w-80 h-80 bg-calm-red rounded-full"></div>
+        
+        <div className="relative nv:ml-20 min-w-[24rem] min-h-[24rem] nv-max:min-h-[19rem] pointer-events-none flex justify-center">
+          <div ref={homePageImageBackgroundRef} className="flex absolute items-center justify-center min-w-[24rem] nv-max:min-w-[18rem] nv-max:min-h-[18rem] min-h-[24rem] bg-canadian-red rounded-full opacity-0 transition-all duration-[1500ms]">
+            <div className="w-[22rem] h-[22rem] nv-max:w-[16.5rem] nv-max:h-[16.5rem] bg-calm-red rounded-full"></div>
           </div>
-          <div ref={homePageImageRef} className='relative'>
+          <div ref={homePageImageRef}>
             <img className={`${homePageImage} delay-75`} src="/images/Home Page/Part 1.png" />
             <img className={`${homePageImage}`} src="/images/Home Page/Part 2.png" />
             <img className={`${homePageImage}`} src="/images/Home Page/Part 3.png" />
@@ -63,7 +64,7 @@ const Home: NextPage = () => {
   );
 };
 
-var homePageImage = classNames('absolute -top-10 -right-[48rem] w-[48rem] min-w-[48rem] transition-all duration-1000');
+var homePageImage = classNames('absolute min-w-[40rem] -right-[40rem] top-0 nv-max:min-w-[30rem] transition-all duration-1000');
 
 export default Home;
 
