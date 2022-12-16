187983761e35d14a1e7f2ed04602d069885935c7
ahmadhaytham <ahm672001@gmail.com>
2022-12-16 10:33:31 -0800

    ss

--- BackEnd/routes/Admin.js
+++ BackEnd/routes/Admin.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const connection = require('../config/database');
-const { viewCourseRequests, grantOrRejectAccess,AcceptOrRejectRefund } = require('../controller/AdminController');
+
 const Admin = require('../models/AdminSchema');
-const {viewCourseRequests,grantOrRejectAccess,viewReportedFunctions,markReportedProblem}= require('../controller/AdminController')
+const {viewCourseRequests,grantOrRejectAccess,viewReportedFunctions,markReportedProblem,AcceptOrRejectRefund}= require('../controller/AdminController')
 
 
 router.get("/", (req, res) => {
